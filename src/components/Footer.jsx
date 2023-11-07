d62f952e0d86b26e9f8ddc7067ac4b5e65138178
iamTanim <mdtanim0195@gmail.com>
2023-11-07 11:15:12 +0600

    navbar are conditional based on login.done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import WebLogo from "./NavBar/WebLogo";
 
 const Footer = () => {
--- src/components/NavBar/NavLinks.jsx
+++ src/components/NavBar/NavLinks.jsx
@@ -1,57 +1,82 @@
 import {NavLink} from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const NavLinks = () => {
-    
-    return (
-        <div className="flex justify-between">
-            <div className="flex gap-3 ">
-                {/* Navbar menu content here */}
-                <NavLink to="/"
-                    className={
-                        ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
-                }>
-                    Home
-                </NavLink>
-                <NavLink to="/all-Jobs"
-                    className={
-                        ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
-                }>
-                    All Jobs
-                </NavLink>
-                <NavLink to="/applied-jobs"
-                    className={
-                        ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
-                }>
-                    Applied Jobs
-                </NavLink>
-                <NavLink to="/add-a-job"
-                    className={
-                        ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
-                }>
-                    Add A Job
-                </NavLink>
-                <NavLink to="/my-jobs"
-                    className={
-                        ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
-                }>
-                    My Jobs
-                </NavLink>
-                <NavLink to="/blogs"
-                    className={
-                        ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
-                }>
-                    Blogs
-                </NavLink>
-                <NavLink to="/user-profile"
-                    className={
-                        ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
-                }>
-                    User Profile
-                </NavLink>
-            </div>
-            
-            {" "} </div>
-    );
+    const {user} = useAuth();
+    return (<div className="flex justify-between"> {/* Navbar menu content here */}
+
+        {
+        user ?. email ? (<div className="flex gap-3 ">
+            <NavLink to="/"
+                className={
+                    ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
+            }>
+                Home
+            </NavLink>
+            <NavLink to="/all-Jobs"
+                className={
+                    ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
+            }>
+                All Jobs
+            </NavLink>
+
+
+            <NavLink to="/blogs"
+                className={
+                    ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
+            }>
+                Blogs
+            </NavLink>
+            <NavLink to="/user-profile"
+                className={
+                    ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
+            }>
+                User Profile
+            </NavLink>
+            <NavLink to="/my-jobs"
+                className={
+                    ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
+            }>
+                My Jobs
+            </NavLink>
+            <NavLink to="/add-a-job"
+                className={
+                    ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
+            }>
+                Add A Job
+            </NavLink>
+            <NavLink to="/applied-jobs"
+                className={
+                    ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
+            }>
+                Applied Jobs
+            </NavLink>
+
+        </div>) : (<div className="flex gap-3 mx-auto">
+            <NavLink to="/"
+                className={
+                    ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
+            }>
+                Home
+            </NavLink>
+            <NavLink to="/all-Jobs"
+                className={
+                    ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
+            }>
+                All Jobs
+            </NavLink>
+
+
+            <NavLink to="/blogs"
+                className={
+                    ({isActive}) => isActive ? "btn btn-accent btn-sm text-white" : "btn btn-neutral btn-sm text-white"
+            }>
+                Blogs
+            </NavLink>
+        </div>)
+    }
+
+        {" "} </div>);
 };
 
 export default NavLinks;
